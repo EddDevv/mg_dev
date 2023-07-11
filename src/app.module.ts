3423a049224c8e831679a54ed61cf095a2704e80
Vladimir <shokilak@gmail.com>
2023-07-11 20:59:59 +0600

    feat: updated app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,42 +1,25 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { SequelizeModule } from '@nestjs/sequelize'
-import { AuthModule } from './domain/auth/auth.module'
-import { BusinessAccountModule } from './domain/business-accounts/business-accounts.module'
-import { BusinessAccount } from './domain/business-accounts/business-account.entity'
-import { Location } from './domain/locations/location.entity'
-import { LocationsModule } from './domain/locations/locations.module'
-import { User } from './domain/users/user.entity'
-import { UserModule } from './domain/users/user.module'
-
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './domain/auth/auth.module';
+import { BusinessAccountModule } from './domain/business-accounts/business-accounts.module';
+import { LocationsModule } from './domain/locations/locations.module';
+import { UserModule } from './domain/users/user.module';
+import { databaseProviders } from './infrastructure/dataSourceOptions';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	controllers: [],
-	providers: [],
-	imports: [
-		ConfigModule.forRoot({
-			envFilePath: `.${process.env.NODE_ENV}.env`
-		}),
-		SequelizeModule.forRoot({
-      dialect: 'postgres',
-			host: process.env.DB_HOST,
-			username: process.env.DB_USERNAME,
-			port: Number(process.env.DB_PORT),
-			password:process.env.DB_PASSWORD,
-			database: process.env.DB_DATABASENAME,
-			models: [
-				User, 
-				BusinessAccount, 
-				Location
-			],
-      autoLoadModels: true,
-      synchronize: true,
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
     }),
-		AuthModule,
-		UserModule,
-		BusinessAccountModule,
-		LocationsModule,
-	]
+    TypeOrmModule.forRootAsync(databaseProviders),
+    AuthModule,
+    UserModule,
+    BusinessAccountModule,
+    LocationsModule,
+  ],
 })
-
 export class AppModule {}
