00cda84f7a0ed18242a92de18213c70408d5bdb2
Vladimir <shokilak@gmail.com>
2023-07-11 21:00:56 +0600

    feat: updated BusinessAccount

--- src/domain/business-accounts/business-accounts.module.ts
+++ src/domain/business-accounts/business-accounts.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common'
-import { SequelizeModule } from '@nestjs/sequelize'
-import { UserModule } from 'src/domain/users/user.module'
-import { BusinessAccountController } from '../../application/controllers/business-accounts.controller'
-import { BusinessAccountService } from './business-accounts.service'
-import { BusinessAccount } from './business-account.entity'
+import { Module } from '@nestjs/common';
+import { UserModule } from 'src/domain/users/user.module';
+import { BusinessAccountController } from '../../application/controllers/business-accounts.controller';
+import { BusinessAccountService } from './business-accounts.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BusinessAccountEntity } from './business-account.entity';
+import { BusinessAccountsRepository } from '../../infrastructure/repositories/business-accounts.repository';
 
 @Module({
-  imports: [SequelizeModule.forFeature([BusinessAccount]), UserModule],
+  imports: [TypeOrmModule.forFeature([BusinessAccountEntity]), UserModule],
   controllers: [BusinessAccountController],
-  providers: [BusinessAccountService]
+  providers: [BusinessAccountService, BusinessAccountsRepository],
 })
 export class BusinessAccountModule {}
--- src/domain/business-accounts/business-accounts.service.ts
+++ src/domain/business-accounts/business-accounts.service.ts
@@ -3,53 +3,61 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
 import { UserService } from 'src/domain/users/user.service';
-import { BusinessAccount } from './business-account.entity';
 import {
   BusinessAccountsCreateRequest,
   BusinessAccountsUpdateRequest,
 } from '../../application/dto/business-accounts/business-accounts.request';
+import { BusinessAccountsRepository } from '../../infrastructure/repositories/business-accounts.repository';
+import { UsersResponse } from '../../application/dto/users/users.response';
+import {
+  BusinessAccount,
+  BusinessAccountEntity,
+} from './business-account.entity';
+import {
+  BusinessAccountsListResponse,
+  BusinessAccountsResponse,
+} from '../../application/dto/business-accounts/business-accounts.response';
 
 @Injectable()
 export class BusinessAccountService {
   constructor(
-    @InjectModel(BusinessAccount)
-    private businessAccountRepository: typeof BusinessAccount,
+    private businessAccountRepository: BusinessAccountsRepository,
     private userService: UserService,
   ) {}
 
-  async create(createBusinessAccountDto: BusinessAccountsCreateRequest) {
-    const userId = createBusinessAccountDto.userId;
-    const isBusiness = await this.businessAccountRepository.findByPk(userId);
+  async create(data: BusinessAccountsCreateRequest): Promise<UsersResponse> {
+    const isBusiness = await this.businessAccountRepository.findOne({
+      where: { userId: data.userId },
+    });
 
     if (!isBusiness) {
-      const businessAccount = await this.businessAccountRepository.create(
-        createBusinessAccountDto,
+      const businessAccount = await this.businessAccountRepository.save(
+        new BusinessAccountEntity(data.businessName),
       );
 
       if (businessAccount) {
-        const user = await this.userService.updateRole(
-          createBusinessAccountDto.userId,
-        );
-        return user;
+        return await this.userService.updateRole(data.userId);
       }
     } else {
       throw new BadRequestException('You already have a business account');
     }
   }
 
-  async findAll(): Promise<BusinessAccount[]> {
-    const businessAccounts = await this.businessAccountRepository.findAll();
-    return businessAccounts;
+  async findAll(): Promise<BusinessAccountsListResponse> {
+    const businessAccounts = await this.businessAccountRepository.find({});
+    return new BusinessAccountsListResponse(businessAccounts);
   }
 
-  async findOne(id: number): Promise<BusinessAccount> {
-    const businessAccount = await this.businessAccountRepository.findByPk(id);
+  async findOne(id: number): Promise<BusinessAccountsResponse> {
+    const businessAccount = await this.businessAccountRepository.findOne({
+      where: { id },
+    });
     if (!businessAccount) {
       throw new NotFoundException('User not found');
     }
-    return businessAccount;
+
+    return new BusinessAccountsResponse(businessAccount);
   }
 
   update(id: number, updateBusinessAccountDto: BusinessAccountsUpdateRequest) {
