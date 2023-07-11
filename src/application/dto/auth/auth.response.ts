5ed02346af7367e086fb56662aca614e99781872
Vladimir <shokilak@gmail.com>
2023-07-11 21:00:26 +0600

    feat: updated auth

--- src/application/dto/auth/auth.response.ts
+++ src/application/dto/auth/auth.response.ts
@@ -1,17 +1,26 @@
-import {ApiProperty} from "@nestjs/swagger";
-
-export class AuthRegisterResponse {
-
-}
+import { ApiProperty } from '@nestjs/swagger';
+import { UsersResponse } from '../users/users.response';
+import { UserEntity } from '../../../domain/users/user.entity';
 
 export class AuthTokensResponse {
-    @ApiProperty()
-    accessToken: string
+  @ApiProperty()
+  accessToken: string;
 
-    @ApiProperty()
-    refreshToken: string
+  @ApiProperty()
+  refreshToken: string;
 }
 
-export class AuthLoginResponse {
+export class AuthRegisterResponse {
+  @ApiProperty()
+  user: UsersResponse;
 
+  @ApiProperty()
+  tokens: AuthTokensResponse;
+
+  constructor(user: UserEntity, tokens: AuthTokensResponse) {
+    this.user = new UsersResponse(user);
+    this.tokens = tokens;
+  }
 }
+
+export class AuthLoginResponse extends AuthRegisterResponse {}
