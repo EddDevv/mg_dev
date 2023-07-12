1f44151d681fba90ed862d782ceef19ca578ec1f
Vladimir <shokilak@gmail.com>
2023-07-12 13:27:55 +0600

    feat: messages of response/exceptions

---
+++ src/config/messages/response.messages.ts
@@ -0,0 +1,20 @@
+export const ResponseMessages = {
+  auth: {
+    register: 'User success registration',
+    login: 'Success login',
+    refreshToken: 'Receive refreshToken',
+  },
+  businessAccount: {
+    create: 'Success creation of business account',
+    findAll: 'Get list of business accounts',
+    findOne: 'Get business account',
+    update: 'Success update business account',
+  },
+  locations: {},
+  user: {
+    findAll: 'Get list of users',
+    findOne: 'Get user data',
+    update: 'Success update',
+    remove: 'Success remove',
+  },
+};
