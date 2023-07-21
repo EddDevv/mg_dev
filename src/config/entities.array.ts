5a54329fa8ac2bec453ee7d63883dd8520427e6a
Vladimir <shokilak@gmail.com>
2023-07-21 16:56:42 +0600

    feat: added like entity

--- src/config/entities.array.ts
+++ src/config/entities.array.ts
@@ -5,6 +5,7 @@ import { SocialLinksEntity } from '../domain/social/social-links.entity';
 import { PostEntity } from 'src/domain/posts/post.entity';
 import { SubscriptionsEntity } from '../domain/subscriptions/subscriptions.entity';
 import { CommentEntity } from 'src/domain/comments/comment.entity';
+import { LikesEntity } from '../domain/likes/likes.entity';
 
 export const EntitiesArray = [
   UserEntity,
@@ -14,4 +15,5 @@ export const EntitiesArray = [
   PostEntity,
   SubscriptionsEntity,
   CommentEntity,
+  LikesEntity,
 ];
