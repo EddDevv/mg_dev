e4d1ae7f592587e2d7ba11060af74486f53c28a5
NeoGil <neogil28@gmail.com>
2023-07-21 16:05:58 +0300

    fix: added interface in portfolio create update and get list request

--- src/application/dto/portfolio/portfolio.request.ts
+++ src/application/dto/portfolio/portfolio.request.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { IPortfolio } from 'src/domain/portfolio/portfolio.entity';
 
-export class PortfolioCreateRequest
+export class PortfolioCreateRequest implements IPortfolio
 {
   @ApiProperty({ example: '2', description: 'The Business id' })
   @IsNotEmpty()
@@ -17,7 +18,7 @@ export class PortfolioCreateRequest
   description: string;
 }
 
-export class PortfolioUpdateRequest
+export class PortfolioUpdateRequest implements Omit<IPortfolio, 'businessId'>
 {
   @ApiProperty({
     example:
@@ -26,4 +27,12 @@ export class PortfolioUpdateRequest
   })
   @IsString()
   description: string;
+}
+
+export class PortfolioGetListRequest
+  implements Omit<IPortfolio, 'description'>
+{
+  @ApiProperty({ example: '2', description: 'The Business id' })
+  @IsNotEmpty()
+  businessId: number;
 }
