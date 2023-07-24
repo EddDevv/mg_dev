c093100b2be446f930385a1712e45ac23c6c3e84
Vladimir <shokilak@gmail.com>
2023-07-24 08:57:49 +0600

    feat: added redoc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { RedocModule } from 'nestjs-redoc';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
@@ -13,7 +14,8 @@ async function start() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api', app, document);
+  // @ts-ignore
+  await RedocModule.setup('/api', app, document, {});
 
   await app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
 }
