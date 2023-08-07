e9e327db8ec66e1530c6deed347e28142fa3d984
perezvonish <shokilak@gmail.com>
2023-08-07 21:33:32 +0500

    feat: 0.0.11

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function start() {
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Masterhub Documentation API')
-    .setVersion('0.0.10')
+    .setVersion('0.0.11')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
