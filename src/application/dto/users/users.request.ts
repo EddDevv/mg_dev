795d357fed541f2abe622ed613c59675a5fe55e0
perezvonish <shokilak@gmail.com>
2023-08-11 21:04:03 +0500

    fix: createdAt on date of birthday

--- src/application/dto/users/users.request.ts
+++ src/application/dto/users/users.request.ts
@@ -69,6 +69,7 @@ export class UserUpdateRequest
       | 'password'
       | 'onlineStatus'
       | 'receiveNotifications'
+      | 'dateOfBirth'
     >
 {
   @ApiProperty({ nullable: true })
@@ -94,10 +95,8 @@ export class UserUpdateRequest
 
   @ApiProperty({ nullable: true })
   @IsOptional()
-  @IsDateString({
-    strict: true,
-  })
-  createdAt: string | null;
+  @IsDateString()
+  dateOfBirth: string | null;
 
   @ApiProperty({ nullable: true })
   @IsOptional()
